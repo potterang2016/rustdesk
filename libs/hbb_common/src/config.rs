a4d84b6fab5b6805abe238c902381fcd80c2bf6b
potterang2016 <78012750+potterang2016@users.noreply.github.com>
2025-01-15 10:58:59 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["192.9.120.22"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.jst.com.sg"];
 pub const PUBLIC_RS_PUB_KEY: &str = "bU7a6oxsWB3PriynQBq02lbgY9pezA9fGMfhOOx8Xws=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
